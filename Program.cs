14c61d3f768fc18c746a7d914161b9b515692866
Ashish Bandiwar <abandiwar@svam.com>
2025-02-02 21:20:38 -0500

    Added changes for Contains Duplicate

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Problem_Solving;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int[] nums = { 1, 2, 1, 2, 2, 2 };
+        Console.WriteLine(ContainsDuplicate(nums));
+    }
+
+    public static bool ContainsDuplicate(int[] nums){
+        HashSet<int> set = new HashSet<int>();
+        foreach(int i in nums){
+            if(set.Contains(i)){
+                return true;
+            }
+            set.Add(i);
+        }
+        return false;
+    }
+}
